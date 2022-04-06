efcec4b19c41b5f3bf6b605b09ef380c3e1bbf68
amiraidenov <103108488+amiraidenov@users.noreply.github.com>
2022-04-06 15:44:41 +0600

    Update glasnie.py

--- glasnie.py
+++ glasnie.py
@@ -1,6 +1,6 @@
 b = input()
 def get_vowels(String):
-    return [each for each in String if each in "aeiou"]
+    return [each for each in String if each in "aeioul"]
 get_vowels("animal") # [a, i, a]
 get_vowels("why") # []
 get_vowels("football") # [o, o, a]
